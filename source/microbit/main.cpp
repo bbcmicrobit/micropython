1d896091935bb135a1a3e0ef68e7649279071492
Damien George <damien.p.george@gmail.com>
2015-09-11 10:07:38 +0100

    Don't print 'P' on MicroPython start up.
    Feedback from teachers showed that printing 'P' is confusing.

--- source/microbit/main.cpp
+++ source/microbit/main.cpp
@@ -4,12 +4,8 @@ extern "C" {
     void mp_run(void);
 }
 
-void app_main()
-{
+void app_main() {
     while (1) {
-        uBit.display.print('P');
-        uBit.sleep(100);
-        uBit.display.print(' ');
         mp_run();
     }
 }
