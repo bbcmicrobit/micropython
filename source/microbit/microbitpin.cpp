7dafb0019c31d8126cb6461ea7a43673cecd17fc
mark <mark@semmle.com>
2015-10-06 14:03:14 +0100

    Flatten inheritance hierarchy of pin types.

--- source/microbit/microbitpin.cpp
+++ source/microbit/microbitpin.cpp
@@ -103,6 +103,89 @@ mp_obj_t microbit_pin_is_touched(mp_obj_t self_in) {
 }
 MP_DEFINE_CONST_FUN_OBJ_1(microbit_pin_is_touched_obj, microbit_pin_is_touched);
 
+STATIC const mp_map_elem_t microbit_dig_pin_locals_dict_table[] = {
+    { MP_OBJ_NEW_QSTR(MP_QSTR_write_digital), (mp_obj_t)&microbit_pin_write_digital_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_read_digital), (mp_obj_t)&microbit_pin_read_digital_obj },
+};
+
+STATIC const mp_map_elem_t microbit_ann_pin_locals_dict_table[] = {
+    { MP_OBJ_NEW_QSTR(MP_QSTR_write_digital), (mp_obj_t)&microbit_pin_write_digital_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_read_digital), (mp_obj_t)&microbit_pin_read_digital_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_write_analog), (mp_obj_t)&microbit_pin_write_analog_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_read_analog), (mp_obj_t)&microbit_pin_read_analog_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_set_analog_period), (mp_obj_t)&microbit_pin_set_analog_period_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_set_analog_period_microseconds), (mp_obj_t)&microbit_pin_set_analog_period_microseconds_obj },
+};
+
+STATIC const mp_map_elem_t microbit_touch_pin_locals_dict_table[] = {
+    { MP_OBJ_NEW_QSTR(MP_QSTR_write_digital), (mp_obj_t)&microbit_pin_write_digital_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_read_digital), (mp_obj_t)&microbit_pin_read_digital_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_write_analog), (mp_obj_t)&microbit_pin_write_analog_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_read_analog), (mp_obj_t)&microbit_pin_read_analog_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_set_analog_period), (mp_obj_t)&microbit_pin_set_analog_period_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_set_analog_period_microseconds), (mp_obj_t)&microbit_pin_set_analog_period_microseconds_obj },
+    { MP_OBJ_NEW_QSTR(MP_QSTR_is_touched), (mp_obj_t)&microbit_pin_is_touched_obj },
+};
+
+STATIC MP_DEFINE_CONST_DICT(microbit_dig_pin_locals_dict, microbit_dig_pin_locals_dict_table);
+STATIC MP_DEFINE_CONST_DICT(microbit_ann_pin_locals_dict, microbit_ann_pin_locals_dict_table);
+STATIC MP_DEFINE_CONST_DICT(microbit_touch_pin_locals_dict, microbit_touch_pin_locals_dict_table);
+
+
+const mp_obj_type_t microbit_dig_pin_type = {
+    { &mp_type_type },
+    .name = MP_QSTR_MicroBitDigitalPin,
+    .print = NULL,
+    .make_new = NULL,
+    .call = NULL,
+    .unary_op = NULL,
+    .binary_op = NULL,
+    .attr = NULL,
+    .subscr = NULL,
+    .getiter = NULL,
+    .iternext = NULL,
+    .buffer_p = {NULL},
+    .stream_p = NULL,
+    .bases_tuple = MP_OBJ_NULL,
+    /* .locals_dict = */ (mp_obj_t)&microbit_dig_pin_locals_dict,
+};
+
+const mp_obj_type_t microbit_ad_pin_type = {
+    { &mp_type_type },
+    .name = MP_QSTR_MicroBitAnalogDigitalPin,
+    .print = NULL,
+    .make_new = NULL,
+    .call = NULL,
+    .unary_op = NULL,
+    .binary_op = NULL,
+    .attr = NULL,
+    .subscr = NULL,
+    .getiter = NULL,
+    .iternext = NULL,
+    .buffer_p = {NULL},
+    .stream_p = NULL,
+    .bases_tuple = MP_OBJ_NULL,
+    /* .locals_dict = */ (mp_obj_t)&microbit_ann_pin_locals_dict,
+};
+
+const mp_obj_type_t microbit_touch_pin_type = {
+    { &mp_type_type },
+    .name = MP_QSTR_MicroBitTouchPin,
+    .print = NULL,
+    .make_new = NULL,
+    .call = NULL,
+    .unary_op = NULL,
+    .binary_op = NULL,
+    .attr = NULL,
+    .subscr = NULL,
+    .getiter = NULL,
+    .iternext = NULL,
+    .buffer_p = {NULL},
+    .stream_p = NULL,
+    .bases_tuple = MP_OBJ_NULL,
+    /* .locals_dict = */ (mp_obj_t)&microbit_touch_pin_locals_dict,
+};
+
 const microbit_pin_obj_t microbit_p0_obj = {{&microbit_touch_pin_type}, &uBit.io.P0};
 const microbit_pin_obj_t microbit_p1_obj = {{&microbit_touch_pin_type}, &uBit.io.P1};
 const microbit_pin_obj_t microbit_p2_obj = {{&microbit_touch_pin_type}, &uBit.io.P2};
