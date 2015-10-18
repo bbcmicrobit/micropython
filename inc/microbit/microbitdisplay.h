1d0814067156fe4f2937f777c1901630ddf69122
Mark Shannon <mark@hotpy.com>
2015-10-18 23:52:05 +0100

    Fix rendering of low brightness pixels.

--- inc/microbit/microbitdisplay.h
+++ inc/microbit/microbitdisplay.h
@@ -2,11 +2,24 @@
 
 typedef struct _microbit_display_obj_t {
     mp_obj_base_t base;
+    uint8_t image_buffer[5][5];
+    uint8_t row_brightness[MICROBIT_DISPLAY_COLUMN_COUNT];
+    uint8_t previous_brightness;
+    /* Current row for strobing */
+    uint8_t strobe_row;
     /* boolean histogram of brightness in buffer */
     uint16_t brightnesses;
-    uint8_t image_buffer[5][5];
+    uint16_t strobe_mask;
+    
+    void advanceRow();
+    void renderRow();
+    inline void setPinsForRow(uint8_t brightness);
+    
 } microbit_display_obj_t;
 
+#define ASYNC_MODE_STOPPED 0
+#define ASYNC_MODE_ANIMATION 1
+#define ASYNC_MODE_CLEAR 2
 
 extern microbit_display_obj_t microbit_display_obj;
 
