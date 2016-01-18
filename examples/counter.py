404ed523bcc503b27480f431dedf34d3731667ea
Joshua Herman <zitterbewegung@gmail.com>
2016-01-18 14:53:35 -0600

    Create counter.py
    Simple counter for the bbc micro micro python for pedagogical purposes.

---
+++ examples/counter.py
@@ -0,0 +1,9 @@
+#A counter for the microbit using the a button.
+
+import microbit
+ctr = 0  # Initialize counter
+while True: # Loop forever
+  if microbit.button_a.is_pressed():   
+    microbit.display.scroll(str(ctr)) #Display counter
+    ctr = ctr + 1  #Increment counter by 
+  microbit.sleep(100)
