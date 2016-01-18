a983b5d4d60577ee7f8c9d66fef977c0dc21f094
Joshua Herman <zitterbewegung@gmail.com>
2016-01-18 16:35:29 -0600

    Update counter.py
    Better formatting.

--- examples/counter.py
+++ examples/counter.py
@@ -1,9 +1,9 @@
-#A counter for the microbit using the a button.
+#A counter for the microbit using the A button.
 
 import microbit
-ctr = 0  # Initialize counter
-while True: # Loop forever
+ctr = 0                               # Initialize counter
+while True:                           # Loop forever
   if microbit.button_a.is_pressed():   
     microbit.display.scroll(str(ctr)) #Display counter
-    ctr = ctr + 1  #Increment counter by one
+    ctr = ctr + 1                     #Increment counter by one
   microbit.sleep(100)
