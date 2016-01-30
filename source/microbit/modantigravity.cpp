e9d176c914dbd9b4ee0143dd7252fb38e40d70c2
JoeGlancy <joe.t.glancy@gmail.com>
2016-01-30 11:40:08 +0000

    Attempt #1

--- source/microbit/modantigravity.cpp
+++ source/microbit/modantigravity.cpp
@@ -3,7 +3,7 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2015 Damien P. George
+ * Copyright (c) 2015-2016 Damien P. George, Joe Glancy
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -36,11 +36,11 @@ extern "C" {
 #define GET_PIXEL(x, y) microbit_display_get_pixel(&microbit_display_obj, x, y)
 #define SET_PIXEL(x, y, v) microbit_display_set_pixel(&microbit_display_obj, x, y, v)
 
-STATIC mp_obj_t antigravity_ascii(void);
+STATIC void antigravity_output_antigravity_ascii()
 
 void antigravity(uint8_t interval = 200 /* ms */) {
-    // move all of the LED's upwards (we can move them in other directions in the future)
-	antigravity_ascii();
+    // move all of the LEDs upwards (we can move them in other directions in the future)
+	antigravity_output_antigravity_ascii();
 
     for (uint8_t iteration = 0; iteration < 5; iteration++) {
         mp_hal_delay_ms(interval);
@@ -66,42 +66,252 @@ void antigravity(uint8_t interval = 200 /* ms */) {
     }
 }
 
-// as best the original antigravity as you're probably going to get
-STATIC mp_obj_t antigravity_ascii(void) {
-    STATIC const char *antigrav =
-"+- xkcd.com/353 ---------------------------------------------------------------+\n"
-"|                                                                              |\n"
-"|                                                              \\0/             |\n"
-"|                                                            /   \\             |\n"
-"|                 You're flying!                        Python!  /|            |\n"
-"|                     How?                                       \\ \\           |\n"
-"|                     /                                                        |\n"
-"|                   0                                                          |\n"
-"|                  /|\\                                                         |\n"
-"|                   |                                                          |\n"
-"|-__-----------____/_\\_______________________________--------------------------|\n"
-"|                                                                              |\n"
-"|                                                                              |\n"
-"+------------------------------------------------------------------------------+\n"
-"+-----------------------+ +-------------------------+ +------------------------+\n"
-"|                       | |    I dunno...       |   | |      I just typed      |\n"
-"|         0             | |  Dynamic typing?   /    | |   import antigravity   |\n"
-"|        _/\\\\_          | |    Whitespace?    /     | |                  |     |\n"
-"|     /     \\//         | |    /             /      | |     That's it?  /      |\n"
-"|   I learned it last   | |   |      Come join us!  | |     /          /       |\n"
-"|   night! Everything   | |   |       Programming   | |    /    ...I also      |\n"
-"|     is so simple!     | |   |      is fun again!  | |   /      sampled       |\n"
-"|            /          | |   |      It's a  whole  | |  |  everything in the  |\n"
-"|           /           | |   |       new  world    | |  |  medicine cabinet   |\n"
-"|     Hello world is    | |   |        up here!     | |  |   for comparison.   |\n"
-"|         just          | |   0                     | |   0         |          |\n"
-"|                       | |  /|\\/                   | |  /|\\    But I think    |\n"
-"| print(\"Hello, world!\")| |   |   \\_ But how are    | |   |     this is the    |\n"
-"|                       | |  / \\     you flying?    | |  / \\      Python.      |\n"
-"+-----------------------+ +-------------------------+ +------------------------+\n";
-    mp_printf(&mp_plat_print, "%s", antigrav);
-    return mp_const_none;
+#define antigravity_output_str(s,l) MP_PLAT_PRINT_STRN(s,l)
+
+STATIC void antigravity_output_char(char c) {
+	MP_PLAT_PRINT_STRN((char*) &c, 1);
 }
+
+STATIC void antigravity_output_x_chars(char c, uint8_t reps) {
+	while (reps--)
+		antigravity_output_char(c);
+}
+
+STATIC void antigravity_output_x_spaces(uint8_t reps) {
+	antigravity_output_x_chars(' ', reps);
+}
+
+STATIC void antigravity_output_6_spaces() {
+	antigravity_output_x_spaces(6);
+}
+
+STATIC void antigravity_output_vbar_newline_spacer() {
+	antigravity_output_str("|\n|", 3);
+}
+
+STATIC void antigravity_output_vbar_spacer() {
+	antigravity_output_str("| |", 3);
+}
+
+STATIC void antigravity_output_vbar_spacer_and_3_spaces() {
+	antigravity_output_vbar_spacer();
+	antigravity_output_x_spaces(3);
+}
+
+STATIC void antigravity_output_x_hyphens(uint8_t reps) {
+	antigravity_output_x_chars('-', reps);
+}
+
+// due to size issues, the strip has been reduced to a load of
+// function calls below.
+STATIC void antigravity_output_antigravity_ascii() {
+	// first line
+	antigravity_output_str("+-xkcd.com/353", 14);
+	antigravity_output_x_hyphens(65);
+	antigravity_output_str("+\n|", 3);
+	// second line
+	antigravity_output_x_spaces(78);
+	antigravity_output_vbar_newline_spacer();
+	// third line
+	antigravity_output_x_spaces(62);
+	antigravity_output_str("\\0/", 3);
+	antigravity_output_x_spaces(13);
+	antigravity_output_vbar_newline_spacer();
+	// fourth line
+	antigravity_output_x_spaces(60);
+	antigravity_output_str("/   \\", 4);
+	antigravity_output_x_spaces(13);
+	antigravity_output_vbar_newline_spacer();
+	// fifth line
+	antigravity_output_x_spaces(17);
+	antigravity_output_str("You're flying!", 14);
+	antigravity_output_x_spaces(24);
+	antigravity_output_str("Python!  /|", 11);
+	antigravity_output_x_spaces(12);
+	antigravity_output_vbar_newline_spacer();
+	// sixth line
+	antigravity_output_x_spaces(21);
+	antigravity_output_str("How?", 4);
+	antigravity_output_x_spaces(39);
+	antigravity_output_str("\\ \\", 3);
+	antigravity_output_x_spaces(11);
+	antigravity_output_vbar_newline_spacer();
+	// seventh line
+	antigravity_output_x_spaces(21);
+	antigravity_output_char('/');
+	antigravity_output_x_spaces(56);
+	antigravity_output_vbar_newline_spacer();
+	// eigth line
+	antigravity_output_x_spaces(19);
+	antigravity_output_char('0');
+	antigravity_output_x_spaces(58);
+	antigravity_output_vbar_newline_spacer();
+	// ninth line
+	antigravity_output_x_spaces(18);
+	antigravity_output_str("/|\\", 3);
+	antigravity_output_x_spaces(57);
+	antigravity_output_vbar_newline_spacer();
+	// tenth line
+	antigravity_output_x_spaces(19);
+	antigravity_output_char('|');
+	antigravity_output_x_spaces(58);
+	antigravity_output_vbar_newline_spacer();
+	// eleventh line
+	antigravity_output_x_hyphens(14);
+	antigravity_output_x_chars('_', 4);
+	antigravity_output_str("/_\\", 3);
+	antigravity_output_x_chars('_', 31);
+	antigravity_output_x_hyphens(26);
+	antigravity_output_vbar_newline_spacer();
+	// twelth line
+	antigravity_output_x_spaces(78);
+	antigravity_output_vbar_newline_spacer();
+	// thirteenth line
+	antigravity_output_x_chars(' ', 78);
+	antigravity_output_str("|\n+", 3);
+	// fourteenth line
+	antigravity_output_x_hyphens(78);
+	antigravity_output_str("+\n+", 3);
+	// fifteenth line
+	antigravity_output_x_hyphens(23);
+	antigravity_output_x_chars('+', 2);
+	antigravity_output_x_hyphens(25);
+	antigravity_output_x_chars('+', 2);
+	antigravity_output_x_hyphens(24);
+	antigravity_output_str("+\n|", 3);
+	// sixteenth line
+	antigravity_output_x_spaces(23);
+	antigravity_output_vbar_spacer();
+	antigravity_output_x_spaces(4);
+	antigravity_output_str("I dunno...", 10);
+	antigravity_output_x_spaces(7);
+	antigravity_output_str("|   | |", 7);
+	antigravity_output_6_spaces();
+	antigravity_output_str("I just typed", 12);
+	antigravity_output_6_spaces();
+	antigravity_output_vbar_newline_spacer();
+	// seventeenth line
+	antigravity_output_x_spaces(9);
+	antigravity_output_char('0');
+	antigravity_output_x_spaces(13);
+	antigravity_output_str("| |   Dynamic typing?    /    | |   import antigravity   |\n|", 60);
+	// eighteenth line
+	antigravity_output_x_spaces(8);
+	antigravity_output_str("_/\\\\_", 5);
+	antigravity_output_x_spaces(10);
+	antigravity_output_vbar_spacer();
+	antigravity_output_x_spaces(4);
+	antigravity_output_str("Whitespace?    /     | |", 24);
+	antigravity_output_x_spaces(18);
+	// nineteenth line (and a bit of the previous one)
+	antigravity_output_str("|     |\n|     /     \\//", 23);
+	antigravity_output_x_spaces(19);
+	antigravity_output_vbar_spacer();
+	antigravity_output_x_spaces(4);
+	antigravity_output_char('/');
+	antigravity_output_x_spaces(13);
+	antigravity_output_char('/');
+	antigravity_output_6_spaces();
+	antigravity_output_str("| |     That's it?  /", 21);
+	antigravity_output_6_spaces();		// candidate for separate function
+	antigravity_output_vbar_newline_spacer();
+	// twentieth line
+	antigravity_output_str("   I learned it last   ", 17);
+	antigravity_output_vbar_spacer_and_3_spaces();
+	antigravity_output_char('|');
+	antigravity_output_6_spaces();
+	antigravity_output_str("Come join us!  | |", 18);
+	antigravity_output_x_spaces(5);
+	antigravity_output_char('/');
+	antigravity_output_x_spaces(10);
+	antigravity_output_char('/');
+	antigravity_output_x_spaces(7);
+	antigravity_output_vbar_newline_spacer();
+	// twenty-first line
+	antigravity_output_str("   night! Everything   ", 7);
+	antigravity_output_vbar_spacer_and_3_spaces();
+	antigravity_output_char('|');
+	antigravity_output_x_spaces(7);
+	antigravity_output_str("Programming   ", 11);
+	antigravity_output_str("| |    /    ...I also", 21);
+	antigravity_output_6_spaces();
+	antigravity_output_vbar_newline_spacer();
+	// twenty-second line
+	antigravity_output_str("     is so simple!     ", 23);
+	antigravity_output_vbar_spacer_and_3_spaces();
+	antigravity_output_char('|');
+	antigravity_output_6_spaces();
+	antigravity_output_str("is fun again!  ", 13);
+	antigravity_output_vbar_spacer_and_3_spaces();
+	antigravity_output_str("/      sampled", 14);
+	antigravity_output_x_spaces(7);
+	antigravity_output_vbar_newline_spacer();
+	// twenty-third line
+	antigravity_output_x_spaces(12);
+	antigravity_output_char('/');
+	antigravity_output_x_spaces(10);
+	antigravity_output_vbar_spacer_and_3_spaces();
+	antigravity_output_char('|');
+	antigravity_output_6_spaces();
+	antigravity_output_str("It's a  whole  | |  everything in the  |\n|", 42);
+	// twenty-fourth line
+	antigravity_output_x_spaces(11);
+	antigravity_output_char('/');
+	antigravity_output_x_spaces(11);
+	antigravity_output_vbar_spacer_and_3_spaces();
+	// a mix-up of lines
+	antigravity_output_str("|       new  world    | |  |  medicine cabinet   |\n|     "
+						   "Hello world is    | |   |        up here!     | |  |   for comparison.   |\n|", 132);
+	antigravity_output_x_spaces(10);
+	antigravity_output_str("just         | |   0", 20);
+	antigravity_output_x_spaces(21);
+	antigravity_output_str("| |   0         |          |\n|", 30);
+	antigravity_output_x_spaces(23);
+	antigravity_output_str("| |  /|\\/", 9);
+	antigravity_output_x_spaces(19);
+	antigravity_output_str("| |  /|\\    But I think    |\n| print(\"Hello, world!\")"
+						   "| |   |   \\_ But how are    | |   |     this is the    |\n|", 111);
+	antigravity_output_x_spaces(23);
+	antigravity_output_str("| |  / \\     you flying?    | |  / \\      Python.      |\n+", 58);
+	antigravity_output_x_hyphens(23);
+	antigravity_output_str("+ +", 3);
+	antigravity_output_x_hyphens(25);
+	antigravity_output_str("+ +", 3);
+	antigravity_output_x_hyphens(24);
+	antigravity_output_str("+\n", 4);
+}
+/* here is the original ASCII comic strip:
++-xkcd.com/353-----------------------------------------------------------------+
+|                                                                              |
+|                                                              \0/             |
+|                                                            /   \             |
+|                 You're flying!                        Python!  /|            |
+|                     How?                                       \ \           |
+|                     /                                                        |
+|                   0                                                          |
+|                  /|\                                                         |
+|                   |                                                          |
+|--------------____/_\_______________________________--------------------------|
+|                                                                              |
+|                                                                              |
++------------------------------------------------------------------------------+
++-----------------------+ +-------------------------+ +------------------------+
+|                       | |    I dunno...       |   | |      I just typed      |
+|         0             | |  Dynamic typing?   /    | |   import antigravity   |
+|        _/\\_          | |    Whitespace?    /     | |                  |     |
+|     /     \//         | |    /             /      | |     That's it?  /      |
+|   I learned it last   | |   |      Come join us!  | |     /          /       |
+|   night! Everything   | |   |       Programming   | |    /    ...I also      |
+|     is so simple!     | |   |      is fun again!  | |   /      sampled       |
+|            /          | |   |      It's a  whole  | |  |  everything in the  |
+|           /           | |   |       new  world    | |  |  medicine cabinet   |
+|     Hello world is    | |   |        up here!     | |  |   for comparison.   |
+|         just          | |   0                     | |   0         |          |
+|                       | |  /|\/                   | |  /|\    But I think    |
+| print("Hello, world!")| |   |   \_ But how are    | |   |     this is the    |
+|                       | |  / \     you flying?    | |  / \      Python.      |
++-----------------------+ +-------------------------+ +------------------------+
+*/
 
 STATIC mp_obj_t antigravity__init__(void) {
     antigravity();
