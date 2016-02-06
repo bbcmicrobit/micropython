60a51dd8d8c06e9df85e5b0a41511edb8d4f6b01
Nicholas H.Tollervey <ntoll@ntoll.org>
2016-02-06 16:36:04 +0000

    Add gesture related clarification to docs and simple example.

--- docs/accelerometer.rst
+++ docs/accelerometer.rst
@@ -38,7 +38,14 @@ Functions
 
     Return the name of the current gesture.
 
-.. py:function:: is_gesture()
+.. note::
+
+    MicroPython understands the following gesture names: ``"up"``, ``"down"``,
+    ``"left"``, ``"right"``, ``"face up"``, ``"face down"``, ``"freefall"``,
+    ``"3g"``, ``"6g"``, ``"8g"``, ``"shake"``. Gestures are always
+    represented as strings.
+
+.. py:function:: is_gesture(name)
 
     Return True or False to indicate if the named gesture is currently active.
 
@@ -55,8 +62,16 @@ Functions
 
     Clears the gesture history.
 
-Example
--------
+Examples
+--------
+
+A fortune telling magic 8-ball. Ask a question then shake the device for an
+answer.
+
+.. include:: ../examples/magic8.py
+    :code: python
+
+Simple Slalom. Move the device to avoid the obstacles.
 
 .. include:: ../examples/simple_slalom.py
     :code: python
---
+++ examples/magic8.py
@@ -0,0 +1,38 @@
+# Magic 8 ball by Nicholas Tollervey. February 2016.
+#
+# Ask a question then shake.
+#
+# This program has been placed into the public domain.
+from microbit import *
+import random
+
+answers = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes, definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it"
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good"
+    "Very doubtful"
+]
+
+while True:
+    display.show('8')
+    if accelerometer.was_gesture('shake'):
+        display.clear()
+        sleep(1000)
+        display.scroll(random.choice(answers))
+    sleep(10)
