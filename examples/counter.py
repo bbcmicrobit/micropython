f4f4e459ea0e7ffbf0a20be08833bda71edf5a38
Joshua Herman <zitterbewegung@gmail.com>
2016-02-07 20:43:35 -0600

    Improved counter.py
    Added more improvements to counter.py

--- examples/counter.py
+++ examples/counter.py
@@ -6,9 +6,10 @@
 """
 
 import microbit
-ctr = 1                               # Initialize counter
-while True:                           # Loop forever
-  ctr = ctr + microbit.button_a.get_presses()   
-  microbit.button_a.reset_presses()   #Get the amount of presses and add it to ctr
-  microbit.display.scroll(str(ctr))   #Display the counter
-  microbit.sleep(100)                 #Sleep for 100ms
+ctr = 0                                         # Initialize counter
+while True:                                     # Loop forever
+    ctr = ctr + microbit.button_a.get_presses() # Count the amount of presses 
+    microbit.button_a.reset_presses()   
+    if ctr > 0:                                 # Only execute if not zero
+        microbit.display.scroll(str(ctr))       # Display the counter
+    microbit.sleep(100)                         # Sleep for 100ms
