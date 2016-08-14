b834b93b9bb2f3f91e05bfee830fe0b35996b397
Mark Basham <mark.basham@diamond.ac.uk>
2016-08-14 21:34:06 +0100

    Add missing comma to answer list
    Does not cause an error, but does cause 2 elements to be unnecessarily concatenated.  Only spotted this as my first random line was effected.

--- examples/magic8.py
+++ examples/magic8.py
@@ -22,7 +22,7 @@ answers = [
     "Better not tell you now",
     "Cannot predict now",
     "Concentrate and ask again",
-    "Don't count on it"
+    "Don't count on it",
     "My reply is no",
     "My sources say no",
     "Outlook not so good",
