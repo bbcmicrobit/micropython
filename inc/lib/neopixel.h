636151c71da23abd5fc1d1ef86116e788303e9ba
Mathis Gerdes <MathisGerdes@gmail.com>
2017-06-22 21:16:26 +0200

    Fix parameter order in documentation
    Make parameter order match the actual function signature.

--- inc/lib/neopixel.h
+++ inc/lib/neopixel.h
@@ -102,10 +102,10 @@ void neopixel_show(neopixel_strip_t *strip);
 /**
   @brief Write RGB value to LED structure
   @param[in] pointer to Strip structure
+	@param[in] LED number (starting at 1)
 	@param[in] red value
 	@param[in] green value
 	@param[in] blue value
-	@param[in] LED number (starting at 1)
   @retval 0 Successful write
   @retval 1 LED number is out of bounds
 */
