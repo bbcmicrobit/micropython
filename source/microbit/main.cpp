921ae012955ce406f0b44788c2e8ee73874bcdf3
Jim Mussared <jim.mussared@gmail.com>
2017-09-14 22:10:00 +1000

    Slightly reduce the heap size when building with newlib >2.4.0
    Fixes #363

--- source/microbit/main.cpp
+++ source/microbit/main.cpp
@@ -6,6 +6,7 @@
 #include "microbit/microbitobj.h"
 #include "microbit/microbitbutton.h"
 #include "microbit/modmusic.h"
+#include "_newlib_version.h"
 
 // Global instances of the DAL components that we use
 MicroBitDisplay ubit_display;
@@ -119,7 +120,13 @@ typedef struct _appended_script_t {
 int main(void) {
     for (;;) {
         extern uint32_t __StackTop;
+#if __NEWLIB__ > 2 || (__NEWLIB__ == 2 && (__NEWLIB_MINOR__ > 4 || (__NEWLIB_MINOR == 4 && __NEWLIB_PATCHLEVEL > 0)))
+        // newlib >2.4.0 uses more RAM for locale data.
+        // https://github.com/bbcmicrobit/micropython/issues/363
+        static uint32_t mp_heap[10052 / sizeof(uint32_t)];
+#else
         static uint32_t mp_heap[10240 / sizeof(uint32_t)];
+#endif
 
         // Initialise memory regions: stack and MicroPython heap
         mp_stack_set_top(&__StackTop);
