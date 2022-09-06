a5692583be001ca2a84b6c9c1263bae5dbcfe710
Carlos Pereira Atencio <carlos@microbit.org>
2022-09-06 19:06:41 +0100

    docs: Rename wave->waveform from SoundEffect.

--- examples/soundeffects.py
+++ examples/soundeffects.py
@@ -10,7 +10,7 @@ audio.play(audio.SoundEffect(
     duration=500,
     vol_start=100,
     vol_end=255,
-    wave=audio.SoundEffect.WAVE_TRIANGLE,
+    waveform=audio.SoundEffect.WAVEFORM_TRIANGLE,
     fx=audio.SoundEffect.FX_VIBRATO,
     shape=audio.SoundEffect.SHAPE_LOG
 ))
@@ -27,13 +27,13 @@ audio.play(my_effect)
 # You can also create a new effect based on an existing one, and modify
 # any of its characteristics via arguments
 my_modified_effect = my_effect.copy()
-my_modified_effect.wave = audio.SoundEffect.WAVE_NOISE
+my_modified_effect.waveform = audio.SoundEffect.WAVEFORM_NOISE
 audio.play(my_modified_effect)
 
 # Use sensor data to modify and play an existing Sound Effect instance
 my_effect.duration = 600
 while True:
-    # int() might be temporarily neededhttps://github.com/microbit-foundation/micropython-microbit-v2/issues/121
+    # int() might be temporarily needed: https://github.com/microbit-foundation/micropython-microbit-v2/issues/121
     my_effect.freq_start = int(scale(accelerometer.get_x(), from_=(-2000, 2000), to=(0, 9999)))
     my_effect.freq_end = int(scale(accelerometer.get_y(), from_=(-2000, 2000), to=(0, 9999)))
     audio.play(my_effect)
@@ -44,7 +44,7 @@ while True:
         display.show(Image("09090:00000:00900:09990:00900"))
         sleep(500)
     elif button_b.is_pressed():
-        # On button B re-enable speaker & play an effect while showing an image
+        # Button B re-enables the speaker & plays an effect while showing an image
         speaker.on()
         audio.play(audio.SoundEffect(), wait=False)
         display.show(Image.MUSIC_QUAVER)
